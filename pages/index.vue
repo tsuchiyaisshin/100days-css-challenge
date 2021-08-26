408c85ed7f8a1318081c842888fab380672799fd
tsuchiyaisshin <tsuchiyaisshin@icloud.com>
2021-08-26 22:42:36 +0900

    feat: baseを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    <el-card></el-card>
+    <el-main>
+      <el-row class="tac">
+        <el-container>
+          <el-col :span="6">
+            <!-- ここにcss -->
+            <el-card></el-card>
+          </el-col>
+        </el-container>
+        <div class="block">
+          <span class="demonstration">Jump to</span>
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage3"
+            :page-size="100"
+            layout="prev, pager, next, jumper"
+            :total="1000">
+          </el-pagination>
+        </div>
+      </el-row>
+    </el-main>
   </div>
 </template>
 
 <script lang="ts">
+import { Component } from "nuxt-property-decorator"
 import Vue from "vue"
 
-export default Vue.extend({})
+@Component
+export default class Default extends Vue {
+}
 </script>
